97aacb2aa4d39f7bc643a24ab8687a8e5708cc92
MaxUdalov <Maxim1994dw@yandex.ua>
2015-07-12 18:39:30 +0300

    v 1.2

---
+++ Lesson_6/Using_generic_Dictionaries/Using_generic_Dictionaries/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Using_generic_Dictionaries
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public Point() { }
+        public override bool Equals(Object point)
+        {
+            if (point is Point)
+            {
+                if (this.X == ((Point)point).X && this.Y == ((Point)point).Y)
+                    return true;
+                else
+                    return false;
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+
+            return string.Format("X = {0}\t Y = {1}", X, Y);
+        }
+        public override int GetHashCode()
+        {
+            return X.GetHashCode() + Y.GetHashCode();
+        }
+    }
+ 
+    class Program
+    {
+        static public void RandomForObjects(Hashtable map, int sum)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < sum; i++)
+                map.Add(new Point(rnd.Next(1, 100), rnd.Next(1, 100)),i);
+        }
+        static void Main(string[] args)
+        {
+            Hashtable Map = new Hashtable();
+            Map.Add(new Point(1,2),"value");
+            Console.Write("\t\tInput date!!!\n\tX = ");
+            int X = int.Parse(Console.ReadLine());
+            Console.Write("\n\tY = ");
+            int Y = int.Parse(Console.ReadLine());
+            bool a = false;
+            foreach (DictionaryEntry s in Map)
+                if (s.Key.Equals(new Point(X, Y)))
+                    a = true;
+            Console.Write("\tAnswer : ");
+            if (a) Console.WriteLine(X);
+            else Console.WriteLine((char)X);
+                Console.ReadLine();
+        }
+    }
+}
